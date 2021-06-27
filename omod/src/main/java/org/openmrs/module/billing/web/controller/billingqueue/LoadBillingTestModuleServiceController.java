5426944afd5b5d037311bcb205bc3e3ff57c6a02
titusowuor30 <titusowuor30@gmail.com>
2021-06-27 16:04:06 +0300

    updating billing module

--- omod/src/main/java/org/openmrs/module/billing/web/controller/billingqueue/LoadBillingTestModuleServiceController.java
+++ omod/src/main/java/org/openmrs/module/billing/web/controller/billingqueue/LoadBillingTestModuleServiceController.java
@@ -1,4 +1,3 @@
-package org.openmrs.module.billing.web.controller.billingqueue;
 
 import org.openmrs.Concept;
 import org.openmrs.Order;
@@ -9,6 +8,10 @@ import org.openmrs.module.hospitalcore.util.RadiologyConstants;
 import org.openmrs.module.hospitalcore.util.RadiologyUtil;
 import org.openmrs.module.hospitalcore.util.TestModel;
 import org.openmrs.module.radiologyapp.util.RadiologyAppUtil;
+
+import org.openmrs.module.hospitalcore.BillingService;
+import org.openmrs.module.hospitalcore.model.OpdTestOrder;
+
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.fragment.FragmentModel;
@@ -25,47 +28,64 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class LoadBillingTestModuleServiceController {
 
-    //load all  billing test modules
-    public List<SimpleObject> getAllTestModules() {
-        RadiologyService radiologyService = Context.getService(RadiologyService.class);
-        Concept investigation = Context.getConceptService().getConcept(investigationId);
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
-        Date orderDate = null;
-        List<SimpleObject> testOrdersInQueue = new ArrayList<SimpleObject>();
-        try {
-            orderDate = dateFormatter.parse(orderDateString);
-            Map<Concept, Set<Concept>> allowedInvestigations = RadiologyAppUtil.getAllowedInvestigations();
-            Set<Concept> allowableTests = new HashSet<Concept>();
-            if (investigation != null) {
-                allowableTests = allowedInvestigations.get(investigation);
-            } else {
-                for (Concept c : allowedInvestigations.keySet()) {
-                    allowableTests.addAll(allowedInvestigations.get(c));
-                }
-            }
-
-            if (currentPage == null)
-                currentPage = 1;
-
-            List<Order> orders = radiologyService.getOrders(orderDate, phrase, allowableTests,
-                    currentPage);
-            List<TestModel> allTestOrders = RadiologyUtil.generateModelsFromOrders(
-                    orders, allowedInvestigations);
-            List<TestModel> tests = new ArrayList<TestModel>();
-
-            for (TestModel testModel : allTestOrders) {
-                //load all radiology testmodules
-                tests.add(testModel);
-            }
-            testOrdersInQueue = SimpleObject.fromCollection(tests, ui, "startDate", "patientIdentifier", "patientName", "gender", "age", "testName", "orderId","status");
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-            logger.error("Error when parsing order date!"+e.getMessage());
+/**
+ * Created by masenohach_5
+ */
+public class GetAllProcessedRadiologyOrdersController{
+    private static Logger logger = LoggerFactory.getLogger(QueueFragmentController.class);
+    public void controller(FragmentModel model) {
+        RadiologyService radiologyService = (RadiologyService) Context.getService(RadiologyService.class);
+        RadiologyDepartment department = radiologyService.getCurrentRadiologyDepartment();
+        if(department != null){
+            Set<Concept> investigations = department.getInvestigations();
+            model.addAttribute("investigations", investigations);
         }
-        return testOrdersInQueue;
-
     }
-}
+
+    public List<SimpleObject> getProcessedRadilogyTests(
+            @RequestParam("paid") Integer paid,
+            @RequestParam(value = "date", required = false) String dateStr,
+            @RequestParam(value = "currentPage", required = false) Integer currentPage,
+            UiUtils ui) {
+
+        //load billing services
+        BillingService billingService = Context.getService(BillingService.class);
+
+        //load billing services
+        //imp -> ()
+
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        Date date = null;
+
+        try {
+            date = sdf.parse(dateStr);
+
+        }catch(ParseException E){
+            E.printStackTrace();
+        }
+
+
+        //return rad orders as queue
+        SimpleDateFormat dateformat=new SimpleDateFormat("dd/MM/yyyy");
+        List<SimpleObject> testRadOrdersInQue = new ArrayList<>();
+        try{
+            if (currentPage==null)
+                currentPage=1;
+            // get paid/processed list orders
+            List<OpdTestOrder> listOfOrders = billingService.listOfOrder(paid,date);
+
+            List<OpdTestOrder> tests = new ArrayList<OpdTestOrder>();
+            for(OpdTestOrder opdTestOrder : listOfOrders){
+                tests.add(OpdTestOrder)//load all tests to list
+            }
+
+            testRadOrdersInQue=SimpleObject.fromCollection(test,ui,"startDate","patientIdentifier","patientName","gender","age","testName","orderId","status");
+
+        }catch(Exception e){
+            e.printStackTrace();
+            logger.error("Error when parsing order date! "+e.getMessage());
+        }
+
+        return testRadOrdersInQue;
+    }
